d5903825b71dd04fe8d779ba4e513ec54886e685
Steve Smith <stevesmith2609@gmail.com>
2015-04-23 15:08:48 +0100

    Install simple form and created article showpages

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+end
