b1af8c9a39c184390a2ceab5b4f61eabea788ec1
Steve Smith <stevesmith2609@gmail.com>
2015-04-23 15:19:04 +0100

    Add users
    Using Devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index
+    @articles = Article.all.order("title ASC")
   end
 
   def show
